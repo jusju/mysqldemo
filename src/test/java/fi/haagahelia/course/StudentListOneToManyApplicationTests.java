bc0cb68ee5fa19ae5bf2053a514ed761cf51e452
jusju <jukka.juslin@haaga-helia.fi>
2024-02-08 10:06:17 +0200

    first commit

---
+++ src/test/java/fi/haagahelia/course/StudentListOneToManyApplicationTests.java
@@ -0,0 +1,14 @@
+package fi.haagahelia.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
